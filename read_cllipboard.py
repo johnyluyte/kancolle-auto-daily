346bc63be3c848c92f9e4d4853c218baf9682a84
Chun Chien <johnyluyte@gmail.com>
2015-04-03 17:55:10 +0800

    add: basic demo for copy data and get data from clipboard in OS X using pyobj

---
+++ read_cllipboard.py
@@ -0,0 +1,18 @@
+from Foundation import *
+from AppKit import *
+
+# Sets the clipboard to a string
+def pbcopy(s):
+ pb = NSPasteboard.generalPasteboard()
+ pb.declareTypes_owner_([NSStringPboardType], None)
+ newStr = NSString.stringWithString_(s)
+ newData = newStr.nsstring().dataUsingEncoding_(NSUTF8StringEncoding)
+ pb.setData_forType_(newData, NSStringPboardType)
+
+# Gets the clipboard contents
+def pbpaste():
+ pb = NSPasteboard.generalPasteboard()
+ content = pb.stringForType_(NSStringPboardType)
+ return content
+
+print pbpaste()
