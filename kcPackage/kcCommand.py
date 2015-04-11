21872a06b3d07948818f60b8317e29994ec31ba2
Chien Chun <johnyluyte@gmail.com>
2015-04-11 13:52:07 +0800

    update: update kcCommand and shell

---
+++ kcPackage/kcCommand.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+"""
+定義各個畫面名稱
+
+-上方-
+戰績: senseki
+友軍:
+圖鑑:
+道具:
+模樣:
+任務: quest
+課金:
+
+-主要-
+主畫面/回主畫面: port
+編成: hensei
+補給: refill
+改裝: kaisou
+入渠: ndock
+工廠: factory
+出擊: fight
+"""
+# 定義各個按鈕的位置
+
+# 遊戲主畫面
+
+"""
+cmd 的第一個 hash key 是目前所在的場景名稱(general 代表所有頁面)
+cmd 的第二個 hash key 是目前所在的場景可執行的指令
+因此，可以用 cmd[目前所在頁面].keys() 來取得目前場景可執行的所有指令
+
+cmd 的 value 是個 tuple 其格式如下
+
+滑鼠點擊乙次：                           (msg, type=0, pos1)
+滑鼠點擊乙次(會切換場景)：               (msg, type=1, pos1)
+一系列的滑鼠點擊：                       (msg, type=2, count, func, pos1, sleep1, pos2, sleep2 ..)
+滑鼠點擊乙次，有 callback：              (msg, type=3, pos1, func)
+滑鼠點擊乙次(會切換場景)，有 callback：  (msg, type=4, pos1, func)
+
+
+@msg    - (str) 顯示的訊息
+@type   - (int) 指令種類
+      0   滑鼠點擊乙次
+      1   滑鼠點擊乙次(會切換場景)
+      2   一系列的滑鼠點擊
+@count  - (int) 滑鼠點擊的次數，當 type == 2 時才有效
+@func   - (str) callback function name，以 None 代表無
+@pos1   - (int list[2]) 滑鼠點擊的座標 [x, y]
+@sleep1 - (int) pos1 點擊完成後，休息幾秒
+(以下 optional)
+@pos2   - 同 pos1
+@sleep2 - 同 sleep1
+@pos3   - 同 pos1
+@sleep3 - 同 sleep1
+
+舉例來說
+
+
+"""
+
+def get_current_avail_cmds():
+    pass
+
+
+
+cmd = dict()
+
+cmd['general'] = dict()
+cmd['general']['ok']        = ("OK", 0, (400,500))
+cmd['general']['left']      = ("Left", 0, (300,350))
+cmd['general']['right']     = ("Right", 0, (517,343))
+cmd['general']['back']      = ("キャンセル", 0, (116,550))
+cmd['general']['c']         = ("キャンセル", 0, cmd['general']['back'][2])
+cmd['general']['jin1']      = ("陣形 - 単縦陣(水雷特化)", 0, (452,279))
+cmd['general']['jin2']      = ("陣形 - 複縦陣(命中特化)", 0, (572,279))
+cmd['general']['jin3']      = ("陣形 - 輪形陣(防御重視)", 0, (694,282))
+cmd['general']['jin4']      = ("陣形 - 梯形陣", 0, (516,446))
+cmd['general']['jin5']      = ("陣形 - 単横陣(対潜特化)", 0, (648,441))
+
+
+
+# 在主要的那五個畫面之間移動時，只需用左邊那些按鈕即可
+cmd['main6']                = dict()
+cmd['main6']['port']        = ("母港へ移動しました", 1, (79,160))
+cmd['main6']['hensei']      = ("編成画面へ移動しました", 1, (29,252))
+cmd['main6']['refill']      = ("補給画面へ移動しました", 1, (29,303))
+cmd['main6']['kaisou']      = ("改装画面へ移動しました", 1, (23,358))
+cmd['main6']['ndock']       = ("入渠画面へ移動しました", 1, (23,410))
+cmd['main6']['factory']     = ("工廠画面へ移動しました", 1, (25,458))
+cmd['main6']['senseki']     = ("戦績画面へ移動しました", 1, (163,155))
+cmd['main6']['quest']       = ("任務画面へ移動しました", 1, (556,152))
+
+# 改變船隻排列的依據
+cmd['main6']['sort']        = ("ソートルールを変更しました", 0, (775,212))
+
+# 跳到第一頁 或 最後一頁
+cmd['main6']['q']           = ("first page を選択しました", 0, (440,557))
+cmd['main6']['w']           = ("last page を選択しました", 0, (716,554))
+
+# 跳到下一頁 或 上一頁
+cmd['main6']['a']           = ("ページを変更しました", 0, (517,552))
+cmd['main6']['s']           = ("ページを変更しました", 0, (549,551))
+cmd['main6']['d']           = ("ページを変更しました", 0, (584,551))
+cmd['main6']['f']           = ("ページを変更しました", 0, (619,556))
+cmd['main6']['g']           = ("ページを変更しました", 0, (641,545))
+
+# 選擇目前頁面第 N 個船
+cmd['main6']['1']           = ("一番目の艦娘を選択しました", 0, (495,240))
+cmd['main6']['2']           = ("二番目の艦娘を選択しました", 0, (497,267))
+cmd['main6']['3']           = ("三番目の艦娘を選択しました", 0, (497,298))
+cmd['main6']['4']           = ("四番目の艦娘を選択しました", 0, (500,334))
+cmd['main6']['5']           = ("五番目の艦娘を選択しました", 0, (499,363))
+cmd['main6']['6']           = ("六番目の艦娘を選択しました", 0, (499,394))
+cmd['main6']['7']           = ("七番目の艦娘を選択しました", 0, (498,429))
+cmd['main6']['8']           = ("八番目の艦娘を選択しました", 0, (498,453))
+cmd['main6']['9']           = ("九番目の艦娘を選択しました", 0, (496,484))
+cmd['main6']['0']           = ("十番目の艦娘を選択しました", 0, (497,511))
+
+
+cmd['port'] = dict()
+cmd['port']['port']         = ("母港へ移動しました", 1, cmd['main6']['port'][2])
+cmd['port']['hensei']       = ("編成画面へ移動しました", 1, (202,238))
+cmd['port']['refill']       = ("補給画面へ移動しました", 1, (80,315))
+cmd['port']['kaisou']       = ("改装画面へ移動しました", 1, (318,315))
+cmd['port']['ndock']        = ("入渠画面へ移動しました", 1, (127,462))
+cmd['port']['factory']      = ("工廠画面へ移動しました", 1, (275,462))
+cmd['port']['fight']        = ("出撃画面へ移動しました", 4, (207,363), "play_voice_('できれば、戦いは避けたいですね')")  # 出擊
+cmd['port']['senseki']      = ("戦績画面へ移動しました", 1, cmd['main6']['senseki'][2])
+cmd['port']['quest']        = ("任務画面へ移動しました", 1, cmd['main6']['quest'][2])
+
+
+
+
+# 補給
+cmd['refill'] = dict()
+for key, value in cmd['main6'].items():
+    cmd['refill'][key] = value
+cmd['refill']['f1']         = ("第一艦隊を選択しました", 0, (150,223))
+cmd['refill']['f2']         = ("第二艦隊を選択しました", 0, (179,220))
+cmd['refill']['f3']         = ("第三艦隊を選択しました", 0, (206,221))
+cmd['refill']['f4']         = ("第四艦隊を選択しました", 0, (239,226))
+cmd['refill']['f5']         = ("編成されていない艦娘たちを選択しました", 0, (268,220))
+cmd['refill']['all']        = ("全艦隊を選択/解除", 0, (121,223))
+cmd['refill']['oil']        = ("燃料のみ補給しました", 0, (641,540))
+cmd['refill']['bullet']     = ("弾薬のみ補給しました", 0, (775,534))
+cmd['refill']['matome']     = ("燃料と弾薬まとめて補給しました", 0, (703,543))
+
+
+# 編成
+cmd['hensei'] = dict()
+for key, value in cmd['main6'].items():
+    cmd['hensei'][key] = value
+# 因為有 はずす 選項的關係，所以有些按鈕要做些微調
+cmd['hensei']['hazusu']     = ("艦娘をはずしました", 0 , (431,234))
+cmd['hensei']['1']          = (cmd['main6']['1'][0], 0 , (469,266))
+cmd['hensei']['2']          = (cmd['main6']['2'][0], 0 , (471,293))
+cmd['hensei']['3']          = (cmd['main6']['3'][0], 0 , (464,325))
+cmd['hensei']['q']          = (cmd['main6']['q'][0], 0 , (435,550))
+cmd['hensei']['w']          = (cmd['main6']['w'][0], 0 , (716,546))
+cmd['hensei']['yes']        = ("変更完了です", 0 , (716,546))
+cmd['hensei']['y']          = cmd['hensei']['yes']
+cmd['hensei']['hai']        = cmd['hensei']['yes']
+
+
+# change fleets 艦隊
+cmd['hensei']['f1']         = (cmd['refill']['f1'][0], 0, (139,223))
+cmd['hensei']['f2']         = (cmd['refill']['f2'][0], 0, (166,222))
+cmd['hensei']['f3']         = (cmd['refill']['f3'][0], 0, (198,221))
+cmd['hensei']['f4']         = (cmd['refill']['f4'][0], 0, (226,222))
+# watch status 船隻狀態
+cmd['hensei']['s1']         = ("旗艦の詳細です", 0, (329,314))
+cmd['hensei']['s2']         = ("二番目の艦娘の詳細です", 0, (668,317))
+cmd['hensei']['s3']         = ("三番目の艦娘の詳細です", 0, (328,429))
+cmd['hensei']['s4']         = ("四番目の艦娘の詳細です", 0, (672,432))
+cmd['hensei']['s5']         = ("五番目の艦娘の詳細です", 0, (327,537))
+cmd['hensei']['s6']         = ("六番目の艦娘の詳細です", 0, (673,540))
+# change ship 切換船隻
+cmd['hensei']['c1']         = ("旗艦を変更します", 0, (413,317))
+cmd['hensei']['c2']         = ("二番目の艦娘を変更します", 0, (757,311))
+cmd['hensei']['c3']         = ("三番目の艦娘を変更します", 0, (411,431))
+cmd['hensei']['c4']         = ("四番目の艦娘を変更します", 0, (751,430))
+cmd['hensei']['c5']         = ("五番目の艦娘を変更します", 0, (410,540))
+cmd['hensei']['c6']         = ("六番目の艦娘を変更します", 0, (752,538))
+
+
+
+# 改裝
+cmd['kaisou'] = dict()
+for key, value in cmd['main6'].items():
+    cmd['kaisou'][key] = value
+cmd['kaisou']['f1']         = (cmd['refill']['f1'][0], 0, (144,212))
+cmd['kaisou']['f2']         = (cmd['refill']['f2'][0], 0, (175,216))
+cmd['kaisou']['f3']         = (cmd['refill']['f3'][0], 0, (201,208))
+cmd['kaisou']['f4']         = (cmd['refill']['f4'][0], 0, (235,211))
+cmd['kaisou']['f5']         = (cmd['refill']['f5'][0], 0, (264,211))
+# 第一二三艦隊的六個人
+cmd['kaisou']['q']          = (cmd['main6']['1'][0], 0, (206,263))
+cmd['kaisou']['w']          = (cmd['main6']['2'][0], 0, (207,314))
+cmd['kaisou']['e']          = (cmd['main6']['3'][0], 0, (209,367))
+cmd['kaisou']['r']          = (cmd['main6']['4'][0], 0, (209,426))
+cmd['kaisou']['t']          = (cmd['main6']['5'][0], 0, (205,479))
+cmd['kaisou']['y']          = (cmd['main6']['6'][0], 0, (207,528))
+
+cmd['kaisou']['a']          = (cmd['main6']['a'][0], 0, (180,548))
+cmd['kaisou']['s']          = (cmd['main6']['s'][0], 0, (210,545))
+cmd['kaisou']['d']          = (cmd['main6']['d'][0], 0, (246,539))
+
+cmd['kaisou']['1']          = (cmd['main6']['1'][0], 0, (211,251))
+cmd['kaisou']['2']          = (cmd['main6']['2'][0], 0, (210,282))
+cmd['kaisou']['3']          = (cmd['main6']['3'][0], 0, (205,316))
+cmd['kaisou']['4']          = (cmd['main6']['4'][0], 0, (205,336))
+cmd['kaisou']['5']          = (cmd['main6']['5'][0], 0, (207,368))
+cmd['kaisou']['6']          = (cmd['main6']['6'][0], 0, (205,399))
+cmd['kaisou']['7']          = (cmd['main6']['7'][0], 0, (203,429))
+cmd['kaisou']['8']          = (cmd['main6']['8'][0], 0, (206,453))
+cmd['kaisou']['9']          = (cmd['main6']['9'][0], 0, (209,484))
+cmd['kaisou']['0']          = (cmd['main6']['0'][0], 0, (205,509))
+# cmd['kaisou']['clear'] = [327,328]   # 裝備數量不同，按鈕的位置也不一樣
+
+
+# 入渠
+cmd['ndock'] = dict()
+for key, value in cmd['main6'].items():
+    cmd['ndock'][key] = value
+cmd['ndock']['dock1']       = ("第一ドックを選択しました", 0, (249,268))
+cmd['ndock']['dock2']       = ("第二ドックを選択しました", 0, (246,342))
+cmd['ndock']['repair']      = ("いますぐ修理しますか？", 0, (690,535))
+cmd['ndock']['hai']         = ("はい、なのです", 0, (503,498))
+cmd['ndock']['iie']         = ("いいえ、キャンセルします", 0, (312,494))
+
+
+# 工廠
+cmd['factory'] = dict()
+for key, value in cmd['main6'].items():
+    cmd['factory'][key] = value
+cmd['factory']['dock1']     = (cmd['ndock']['dock1'][0], 0, (614,280))
+cmd['factory']['dock2']     = (cmd['ndock']['dock2'][0], 0, (622,360))
+cmd['factory']['kaitai']    = ("解体されるのは悲しいことです", 0, (234,347))
+cmd['factory']['kaihatu']   = ("新しい装備を開発します", 0, (214,431))
+cmd['factory']['haiki']     = ("古い装備を廃棄します", 0, (223,509))
+cmd['factory']['sort']      = (cmd['main6']['sort'][0], 0, (586,211))
+cmd['factory']['destory']   = ("Hastala Vista, Baby!", 0, (699,533))
+
+cmd['factory']['q']         = (cmd['main6']['q'][0], 0, (218,550))
+cmd['factory']['w']         = (cmd['main6']['w'][0], 0, (497,552))
+cmd['factory']['a']         = (cmd['main6']['a'][0], 0, (297,549))
+cmd['factory']['s']         = (cmd['main6']['s'][0], 0, (332,552))
+cmd['factory']['d']         = (cmd['main6']['d'][0], 0, (364,551))
+cmd['factory']['f']         = (cmd['main6']['f'][0], 0, (395,551))
+cmd['factory']['g']         = (cmd['main6']['q'][0], 0, (422,553))
+cmd['factory']['1']         = (cmd['main6']['1'][0], 0, (294,237))
+cmd['factory']['2']         = (cmd['main6']['2'][0], 0, (296,266))
+cmd['factory']['3']         = (cmd['main6']['3'][0], 0, (297,297))
+cmd['factory']['4']         = (cmd['main6']['4'][0], 0, (296,333))
+cmd['factory']['5']         = (cmd['main6']['5'][0], 0, (296,362))
+cmd['factory']['6']         = (cmd['main6']['6'][0], 0, (296,395))
+cmd['factory']['7']         = (cmd['main6']['7'][0], 0, (295,424))
+cmd['factory']['8']         = (cmd['main6']['8'][0], 0, (296,454))
+cmd['factory']['9']         = (cmd['main6']['9'][0], 0, (294,482))
+cmd['factory']['0']         = (cmd['main6']['0'][0], 0, (295,518))
+
+
+
+# 出擊
+cmd['fight'] = dict()
+for key, value in cmd['main6'].items():
+    cmd['fight'][key] = value
+cmd['fight']['pve']         = ("一番目の任務を選択しました", 0, (232,330))
+cmd['fight']['pvp']         = ("一番目の任務を選択しました", 0, (463,331))
+cmd['fight']['pvt']         = ("一番目の任務を選択しました", 0, (682,330))
+
+
+
+# 任務一覽第 N 頁按鈕位置
+cmd['quest'] = dict()
+cmd['quest']['port'] = cmd['main6']['port']
+cmd['quest']['a']           = (cmd['main6']['a'][0], 0, (355,563))
+cmd['quest']['s']           = (cmd['main6']['s'][0], 0, (406,564))
+cmd['quest']['d']           = (cmd['main6']['d'][0], 0, (455,563))
+cmd['quest']['f']           = (cmd['main6']['f'][0], 0, (514,563))
+cmd['quest']['g']           = (cmd['main6']['g'][0], 0, (563,561))
+cmd['quest']['1']           = ("一番目の任務を選択しました", 0, (739,243))
+cmd['quest']['2']           = ("二番目の任務を選択しました", 0, (739,307))
+cmd['quest']['3']           = ("三番目の任務を選択しました", 0, (739,376))
+cmd['quest']['4']           = ("四番目の任務を選択しました", 0, (743,443))
+cmd['quest']['5']           = ("五番目の任務を選択しました", 0, (746,519))
+# cmd['quest']['c'] = cmd['main6']['port'][2]
+# btn_pages = [btn_page1, btn_page2, btn_page3, btn_page4, btn_page5]
+
+
+
--- kcPackage/shell.py
+++ kcPackage/shell.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import kcUtil as g
-import kcGPS as gps
-
-
+import kcCommand as c
 
 """
 統一選取艦隊的地方用 f1 ~ f5
@@ -23,43 +21,18 @@ clear3
 clear4
 clear1
 修正 f1~f5
-"""
 
+quest+ q,w
+"""
 
 
 def init():
-    g.gprint("あははは、びっくりしたのです。")
-    g.gprint("あ、提督さん、ごめんなさいなのです。")
-    g.gprint("私たちは今どこにいますか？")
-    # inp = raw_input("電：提督、ご命令ください\n > ")
+    g.gprint("あわわわ、びっくりしたのです。")
     change_place("hensei")
-    g.gprint("母港ですね、わかりました。")
-
-
-def error_cmd_goto():
-    g.gprint(C_LIGHTRED + "unknown argv for goto: " + C_DEFAULT)
-    msg = "available places for cmd : \n"
-    for item in gps.pos[g.place]:
-     msg += item + " "
-    g.gprint(msg)
 
 
 
-
-
-"""
-指令集：
-
-
-修改目前所在頁面
-place (port, hensei, refill, kaisou, ndock, factory, fight, senjyou)
-
-移動到某個頁面
-(port, hensei, refill, kaisou, ndock, factory, fight)
-
-"""
-
-cmd_no_arg = gps.pos['general'].keys()
+cmd_no_arg = c.cmd['general'].keys()
 cmd_with_arg = ['place']
 avail_cmds = list()
 
@@ -70,16 +43,14 @@ cmd_which_change_place = ['port', 'hensei', 'refill', 'kaisou', 'ndock', 'factor
 def update_avail_cmds():
     global avail_cmds
     avail_cmds = list()
-    avail_cmds = gps.pos[g.place].keys()
+    avail_cmds = c.cmd[g.place].keys()
     avail_cmds += cmd_no_arg + cmd_with_arg
 
 
 def change_place(new_place):
-    g.gprint(gps.pos[g.place][new_place][0])
+    g.gprint(c.cmd[g.place][new_place][0])
     g.place = new_place
     update_avail_cmds()
-    # g.gprint("私たちは今は " + g.place + " にいます")
-
 
 
 # http://apple.stackexchange.com/questions/9821/can-i-make-my-mac-os-x-terminal-color-items-according-to-syntax-like-the-ubuntu
@@ -88,30 +59,30 @@ C_GREEN="\033[32m"
 C_LIGHTGREEN="\033[1;32m"
 C_LIGHTRED="\033[1;31m"
 
-""""""
+
 
 
 init()
 update_avail_cmds()
 
 while True:
-    inp = raw_input(C_LIGHTGREEN + "電：提督、ご命令を" + C_DEFAULT + " > ")
+    tmp = raw_input(C_LIGHTGREEN + "電：提督、ご命令を" + C_DEFAULT + " > ")
 
-    if inp == "exit" or inp =="bye":
+    if tmp == "exit" or tmp =="bye":
         g.gprint("お疲れ様でした、明日も頑張ってください。")
         exit() # Exit
-    elif inp == "cmd" or inp == '?':
+    elif tmp == "cmd" or tmp == '?':
         print avail_cmds
         continue
-    elif inp == "":
-        inp = "ok"
+    elif tmp == "":
+        tmp = "ok"
 
-    argv = inp.split()
-    cmd = argv[0]
+    argv = tmp.split()
+    inp = argv[0]
 
     # 檢查是否為合法的指令
-    if cmd not in avail_cmds:
-        g.gprint(C_LIGHTRED + "unknown command: " + cmd + C_DEFAULT)
+    if inp not in avail_cmds:
+        g.gprint(C_LIGHTRED + "unknown command: " + inp + C_DEFAULT)
         # msg = "電：available commands: \n"
         # for item in avail_cmds:
         #     msg += item + " "
@@ -120,7 +91,7 @@ while True:
 
     # 檢查參數
     # 別忘了這些 if elif 的順序也代表了指令優先順序
-    if cmd == 'place':
+    if inp == 'place':
         # 印出目前畫面位置
         if len(argv) == 1:
             g.gprint("私たちは今は " + g.place + " にいます")
@@ -128,21 +99,21 @@ while True:
         else:
             change_place(argv[1])
     # 切換場景的指令（編成、補給、工廠..）
-    elif cmd in cmd_which_change_place:
+    elif inp in cmd_which_change_place:
         g.get_focus_game()
-        g.click_no_wait(gps.pos[g.place][cmd][1])
+        g.click_no_wait(c.cmd[g.place][cmd][1])
         change_place(cmd)
         g.get_focus_terminal()
     # 每個場景自己的專屬指令（f1, f2, c1, s1, a, s, d, f..）
-    elif cmd in gps.pos[g.place].keys():
+    elif inp in c.cmd[g.place].keys():
         g.get_focus_game()
-        g.click_no_wait(gps.pos[g.place][cmd])
+        g.click_no_wait(c.cmd[g.place][cmd])
         g.get_focus_terminal()
     # 到處都可下的一般指令（ok, c, ..）
-    elif cmd in cmd_no_arg:
+    elif inp in cmd_no_arg:
         g.get_focus_game()
-        g.gprint(gps.pos['general'][cmd][0])
-        g.click_no_wait(gps.pos['general'][cmd][1])
+        g.gprint(c.cmd['general'][cmd][0])
+        g.click_no_wait(c.cmd['general'][cmd][1])
         g.get_focus_terminal()
     else:
         g.gprint(C_LIGHTRED + "ELSEESSESLEE unknown command: " + cmd + C_DEFAULT)
