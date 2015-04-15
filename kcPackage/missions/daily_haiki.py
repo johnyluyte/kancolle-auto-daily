1a7bd2159dc8ba73d7c6284062783cd644326b81
Chun Chien <johnyluyte@gmail.com>
2015-04-15 14:40:16 +0800

    add: add new missions

---
+++ kcPackage/missions/daily_haiki.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+import time
+import datetime
+
+m = PyMouse()
+k = PyKeyboard()
+
+
+# - - - Global Variables - - -
+# 每個步驟與伺服器之間，等待的秒數（也就是預期的網路延遲時間）
+gl_lag = 7
+# Get focus off terminal
+m.click(250, 350, 1)
+
+
+# 將伺服器延遲時間算入的最普通的正常點擊方式
+def click_then_wait(position, sleep_sec):
+    """
+    點擊 position 之後睡眠 sleep_sec 秒，給予伺服器緩衝時間
+    """
+    click_no_wait(position)
+    time.sleep(sleep_sec)
+def click_no_wait(position):
+    print get_time_stamp() + "click_no_wait(): [" + str(position[0]) + "," + str(position[1]) + "]"
+    m.click(position[0], position[1])
+
+def get_time_stamp():
+    current_time = str(datetime.datetime.now()).split('.')[0] # 2015-03-29 20:18:08
+    return "[" + current_time[5:] + "] " # [03-29 20:18:08]
+
+def haiki(item_position):
+    click_no_wait(item_position)
+    click_then_wait([706,529], gl_lag)
+
+def main():
+    # 只需要刪掉第一個就好，因為刪掉之後，後面會自動補上
+    for i in range(5):
+        haiki([190,238])
+    print 'haiki 5 items'
+    # haiki([191,267])
+    # haiki([189,300])
+    # haiki([188,332])
+    # haiki([189,362])
+    # haiki([188,391])
+    # haiki([189,420])
+    # haiki([190,447])
+    # haiki([188,481])
+    # haiki([189,511])
+
+main()
+
---
+++ kcPackage/missions/jin1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+import time
+import datetime
+
+m = PyMouse()
+k = PyKeyboard()
+
+
+# - - - Global Variables - - -
+# 每個步驟之間，等待的秒數（也就是預期的網路延遲時間）
+gl_lag = 4
+
+# 將伺服器延遲時間算入的最普通的正常點擊方式
+def click_then_wait(position, sleep_sec):
+    """
+    點擊 position 之後睡眠 sleep_sec 秒，給予伺服器緩衝時間
+    """
+    print get_time_stamp() + "click_then_wait(): [" + str(position[0]) + "," + str(position[1]) + "]"
+    m.click(position[0], position[1])
+    time.sleep(sleep_sec)
+
+
+def get_time_stamp():
+    current_time = str(datetime.datetime.now()).split('.')[0] # 2015-03-29 20:18:08
+    return "[" + current_time[5:] + "] " # [03-29 20:18:08]
+
+
+def main():
+    while True:
+        click_then_wait((451,278), gl_lag)
+        # 燃料低於 threshold
+        # 或是船艦超過 97 隻就停止練等
+
+main()
+
