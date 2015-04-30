4d8e6daf25ea26f5765c73111c4dbd62e463aa51
Chien Chun <johnyluyte@gmail.com>
2015-04-30 20:50:08 +0800

    other: update kanpe

---
+++ chaban/kanpei.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import time
+import sys
+
+def append_color(msg, color='default'):
+    return '{}{}{}'.format(COLOR[color], msg, COLOR['default'])
+
+COLOR = {
+    'default' : '\033[m',
+    'green'   : '\033[1;32m',
+    'red'     : '\033[1;31m',
+    'yellow'  : '\033[1;33m',
+    'cyan'    : '\033[1;36m'
+}
+
+continents = [
+    ('Africa',0.017),
+    ('Antarctica',0.006),
+    ('Asia',0.026),
+    ('Australia',0.008),
+    ('Europe',0.017),
+    ('North America',0.015),
+    ('South America',0.012)
+]
+
+print 'Searching for the most handsome guy:'
+
+for continent in continents:
+    for i in xrange(101):
+        time.sleep(continent[1])
+        sys.stdout.write("\r{color:>14s} {subject:.<20s}[{oct:<10}]".format(
+            color   = append_color(str(i)+'%', 'cyan'),
+            subject = continent[0],
+            oct     = '#'*(i/10)
+            )
+        )
+        sys.stdout.flush()
+    print
+
